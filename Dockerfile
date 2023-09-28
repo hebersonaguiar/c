4bbf839ffdf4109b485fb5d9c20ff4dd89a6c808
hebersonaguiar <hebersonaguiar@gmail.com>
2023-09-27 22:30:50 -0300

    New feature

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM ubuntu:16.04
+FROM python:3.8.1-slim
 
 RUN apt-get update && apt-get install -y python python-pip
 
-RUN pip install flask
+RUN pip install flask flask_jsonpify flask_restful flask_cors
 
 COPY app.py /opt/
 
--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from flask_jsonpify import jsonify
+from flask_restful import Api
+from flask_cors import CORS
+
 app = Flask(__name__)
+api = Api(app)
+CORS(app, resources={r"/": {"origins": ""}})
+app.secret_key = "7pI.X7<w-qqE&a" 
 
 @app.route("/")
 def main():
     return "Welcome Bahia!"
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+@app.route("/health") 
+def health():
+    data = '{"health": "ok"}' 
+    return jsonify(data), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
